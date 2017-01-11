cfb63a5d2127ec604c340a93ca394f0501979ec8
Alejandro <alejandrombc@gmail.com>
2017-01-10 22:53:41 -0400

    Agregar nuevas vistas de eliminar+recuperar con su logica

--- sist_gest.py
+++ sist_gest.py
@@ -1,10 +1,14 @@
+# encoding=utf-8
 from flask import Flask, render_template, request, url_for, redirect, session
 from flaskext.mysql import MySQL
 import hashlib
 
 app = Flask(__name__)
 mysql = MySQL()
-
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = '123'
+app.config['MYSQL_DATABASE_DB'] = 'don_bosco'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
 
 app.secret_key = "Estodeberiaserandom"
@@ -26,13 +30,6 @@ def index():
 
 	return render_template("index.html", err=error)
 
-@app.route('/conectar')
-def users():
-	cursor = mysql.connect().cursor()
-	cursor.execute("SELECT * from prueba")
-	data = str(cursor.fetchone())
-	return (data)
-
 @app.route('/seleccion')
 def seleccionar():
 	if session.get('logged_in'):
@@ -50,7 +47,7 @@ def agregar():
 		con = mysql.connect()
 		cursor = con.cursor()
 		if (inicio[0] == '2') and (inicio[1] == '0') and (final[0] == '2') and (final[1] == '0') and (len(final)==4) and (len(inicio)==4) and (final > inicio):
-			cursor.execute("""INSERT INTO anos_escolares (inicial, final) VALUES (%s, %s)""", (inicio,final))
+			cursor.execute("""INSERT INTO anos_escolares (inicial, final, eliminada) VALUES (%s, %s, '0')""", (inicio,final))
 			con.commit()
 			session['logged_in'] = "newform" 
 			return redirect(url_for('seleccionar'))
@@ -60,16 +57,56 @@ def agregar():
 	else:
 		return render_template("agregar_ano.html")
 
+
+@app.route('/estudiantes_ano')
+def def_estudiantes():
+	print("NET")
+	return render_template("estudiantes.html")
+
+
 # Al darle a seleccionar busco todos los anos y los mando al html
 @app.route('/seleccionar')
 def seleccion_ano():
 	cursor = mysql.connect().cursor()
-	cursor.execute("SELECT * from anos_escolares")
+	cursor.execute("SELECT * from anos_escolares WHERE eliminada='0'")
 	data = cursor.fetchall()
-	print(data[0][1])
 	return render_template("escoger_ano.html", datos=data)
 
 
+@app.route('/eliminar')
+def eliminar_ano():
+	cursor = mysql.connect().cursor()
+	cursor.execute("SELECT * from anos_escolares WHERE eliminada='0'")
+	data = cursor.fetchall()
+	return render_template("eliminar_ano.html", datos=data)	
+
+
+@app.route('/eliminar_ano', methods=['POST'])
+def eliminar_anos():
+	real_id = request.form['real_id']
+	conn = mysql.connect()
+	cursor = conn.cursor()
+	cursor.execute("UPDATE anos_escolares SET eliminada = 1 WHERE ID=%s",(int(real_id)))
+	conn.commit()
+	return redirect(url_for('eliminar_ano'))
+
+
+@app.route('/recuperar')
+def recuperar_ano():
+	cursor = mysql.connect().cursor()
+	cursor.execute("SELECT * from anos_escolares WHERE eliminada='1'")
+	data = cursor.fetchall()
+	return render_template("recuperar_ano.html", datos=data)		
+
+@app.route('/recuperar_ano', methods=['POST'])
+def recuperar_anos():
+	real_id = request.form['real_id']
+	conn = mysql.connect()
+	cursor = conn.cursor()
+	cursor.execute("UPDATE anos_escolares SET eliminada = 0 WHERE ID=%s",(int(real_id)))
+	conn.commit()
+	return redirect(url_for('recuperar_ano'))			
+
 
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True, port=3000)
---
+++ static/js/estudiantes.js
@@ -0,0 +1,74 @@
+ $(document).ready(function() {
+
+            $("#abrirModal").on('click', function() {
+
+                if($("#inicial").val() != "" && $("#final").val() != "")
+
+                if (validateForm()) $("#myModal").modal('show');
+                $("#confir").text("¿Está seguro de agregar el año escolar "+ $("#inicial").val() + " - " + $("#final").val() + "?");
+
+                $("#realSumbit").on('click', function(){
+                  $("#contact").submit();
+                });
+
+            });
+
+            $( "#tabs" ).tabs();
+            $( ".btn" ).button();
+            $("#externas").tabs();
+            $("#vertical").tabs().addClass("ui-tabs-vertical");
+            $("#vertical > ul > li").removeClass("ui-corner-top").addClass("ui-corner-left");
+            $("#internasA").tabs();
+            $("#internasB").tabs();
+            $("#internasC").tabs();
+
+            $("body").on('click', function(){
+                if (($("#tocheck").hasClass("open"))) {
+                    $("#SUPERparche").css({ display: "none" });
+                }
+            });
+
+            $("#parchesito").on('click', function(){
+                if (($("#tocheck").hasClass("open"))) {
+                    $("#SUPERparche").css({ display: "none" });
+                }
+                else $("#SUPERparche").css({ display: "block" });
+            });
+
+            $("#ocultar").on('click', function(){
+                if (($("#tocheck").hasClass("open"))) {
+                    $("#SUPERparche").css({ display: "none" });
+                }
+                else $("#SUPERparche").css({ display: "block" });
+            });
+
+
+        });
+
+        function validateForm() {
+
+            var inicio = document.getElementById("inicial").value;
+            var final = document.getElementById("final").value;
+            if ((inicio[0] == '2') && (inicio[1] == '0') && (final[0] == '2') && (final[1] == '0') && (final.length == 4) && (inicio.length == 4) && (final > inicio))
+                return true;
+            return false;
+
+        }
+
+        function cambiarValor(valor) {
+            var text = $("#curso_actual").text()
+            if(text != valor){
+                $("#curso_actual").html(valor);
+                // AQUI HAY QUE CAMBIAR VALORES PORQUE CAMBIO EL TEXTO
+            }
+
+        }
+
+        function newActive(clase) {
+            $('#4to').removeClass('active')
+            $('#5to').removeClass('active')
+            $('#6to').removeClass('active')
+            $('#'+clase).addClass('active')
+
+            // AQUI HAY QUE ACTUAlIZAR VALORES PORQUE CAMBIO EL ANO ESCOLAR
+        }
