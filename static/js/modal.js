f36a574d7bcd3ef5d500c0818163945900f77378
Alejandro Barone <alejandrombc@gmail.com>
2017-01-11 21:19:15 -0400

    Agregar nuevo modal.js

---
+++ static/js/modal.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+    $("#abrirModal").on('click', function() {
+      if($("#inicial").val() != "" && $("#final").val() != "")
+        if (validateForm()) $("#myModal").modal('show');
+        $("#confir").text("¿Está seguro de agregar el año escolar "+ $("#inicial").val() + " - " + $("#final").val() + "?");
+
+    });
+
+});
+
+function validateForm() {
+  var inicio = document.getElementById("inicial").value;
+  var final = document.getElementById("final").value;
+  if ((inicio[0] == '2') && (inicio[1] == '0') && (final[0] == '2') && (final[1] == '0') && (final.length == 4) && (inicio.length == 4) && (final > inicio))
+    return true;
+  return false;
+}
+
+
+function openModal(inicial, final, id) {
+  $("#myModal").modal('show');
+  $("#id_ano").val(id);
+  $("#confir").text("¿Está seguro de eliminar el año escolar "+ inicial + " - " + final + "?");
+}
+
+
+function openModal_Rec(inicial, final, id) {
+  $("#myModal").modal('show');
+  $("#id_ano").val(id);
+  $("#confir").text("¿Está seguro de recuperar el año escolar "+ inicial + " - " + final + "?");
+}
