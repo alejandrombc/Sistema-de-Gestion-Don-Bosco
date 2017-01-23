5c7a382e69b8eefdcfef4ee380c1a9cb0ecbea5a
Alejandro Barone <alejandrombc@gmail.com>
2017-01-23 10:21:42 -0400

    Agregar eliminada en el Query

--- consultas.sql
+++ consultas.sql
@@ -3,4 +3,4 @@ drop table if exists anos_escolares;
 
 CREATE TABLE anos_escolares (ID int NOT NULL AUTO_INCREMENT, inicial VARCHAR(4), final VARCHAR(4), eliminada BOOL NOT NULL DEFAULT '0', PRIMARY KEY (ID));
 
-CREATE TABLE secciones (ID int NOT NULL AUTO_INCREMENT, ano VARCHAR(6), curso VARCHAR(20), ano_escolar VARCHAR(10), secciones VARCHAR(2), PRIMARY KEY (ID));
+CREATE TABLE secciones (ID int NOT NULL AUTO_INCREMENT, ano VARCHAR(6), curso VARCHAR(20), ano_escolar VARCHAR(10), secciones VARCHAR(2), eliminada BOOL NOT NULL DEFAULT '0', PRIMARY KEY (ID));
