aba7c41636c640e3293f87ce07215234a1a93e50
Alejandro <alejandrombc@gmail.com>
2017-03-15 18:23:51 -0400

    Update a front de enviar correo

--- static/js/enviar_correo.js
+++ static/js/enviar_correo.js
@@ -20,12 +20,6 @@
 		  });
 		});
 
-		$( "#spoiler4" ).click(function() {
-		  $( "#Personal" ).slideToggle( "slow", function() {
-
-		  });
-		});
-
 		$(".group-span-filestyle input-group-btn").css("z-index",0)
 
 
@@ -81,127 +75,119 @@
 	 	  // Leer secciones 
 	 	 var data = Retornar2();
 			//  Llenamos las secciones de 4to Año 
-			var string = '<table class="table" style="background-color: #FFFFFF;"><tbody><tr><td><b>Tecnología Gráfica:</b></td>'
+			var string = '<table class="table" style="background-color: #FFFFFF;"><tbody><tr><td><b>4to Año: Tecnologia Grafica:</b></td>'
 			for ( i = 0; i< data["1"];i++) {
-				string = string + '<td><label class="checkbox-inline"><input type="checkbox" id="1_' + (i+1) +'"> Sección: ' + TransSeccion(i+1).toString() +' </label></td>';
+				string = string + '<td><label class="checkbox-inline"><input type="checkbox" id="1_' + (i+1) +'"> Seccion: ' + TransSeccion(i+1).toString() +' </label></td>';
 			}
-			string = string + '</tr><tr><td><b>Mecánica:</b></td>';
+			string = string + '</tr><tr><td><b>4to Año: Mecanica:</b></td>';
 			for ( i = 0; i< data["2"];i++) {
-				string = string + '<td><label class="checkbox-inline"><input type="checkbox" id="2_' + (i+1) +'"> Sección: ' + TransSeccion(i+1).toString() +' </label></td>';
+				string = string + '<td><label class="checkbox-inline"><input type="checkbox" id="2_' + (i+1) +'"> Seccion: ' + TransSeccion(i+1).toString() +' </label></td>';
 			}
-			string = string + "</tr><tr><td><b>Electrónica:</b></td>";
+			string = string + "</tr><tr><td><b>4to Año: Electronica:</b></td>";
+			
 			for ( i = 0; i< data["3"];i++) {
-				string = string + '<td><label class="checkbox-inline"><input type="checkbox" id="3_' + (i+1) +'"> Sección: ' + TransSeccion(i+1).toString() +' </label>';
+				string = string + '<td><label class="checkbox-inline"><input type="checkbox" id="3_' + (i+1) +'"> Seccion: ' + TransSeccion(i+1).toString() +' </label></td>';
 			}
-			string = string + "</tr><tr><td><b>Contabilidad:</b></td>";
+			string = string + "</tr><tr><td><b>4to Año: Contabilidad:</b></td>";
 			for ( i = 0; i< data["4"];i++) {
-				string = string + '<td><label class="checkbox-inline"><input type="checkbox" id="4_' + (i+1) +'"> Sección: ' + TransSeccion(i+1).toString() +' </label></td>';
+				string = string + '<td><label class="checkbox-inline"><input type="checkbox" id="4_' + (i+1) +'"> Seccion: ' + TransSeccion(i+1).toString() +' </label></td>';
 			}
-			string = string + '</tr></table>'		
+			string = string + '</tr></tbody></table>'		
 			$("#Secciones").html(string);
-
-			var string = '<table class="table" style="background-color: #FFFFFF;"><tbody><tr><td><b>Tecnología Gráfica:</b></td>'
+			//  Llenamos las secciones de 5to Año 
+			var string = '<table class="table" style="background-color: #FFFFFF;"><tbody><tr><td><b>5to Año: Tecnologia Grafica:</b></td>'
 			for ( i = 0; i< data["5"];i++) {
-				string = string + '<td><label class="checkbox-inline"><input type="checkbox" id="5_' + (i+1) +'"> Sección: ' + TransSeccion(i+1).toString() +' </label></td>';
+				string = string + '<td><label class="checkbox-inline"><input type="checkbox" id="5_' + (i+1) +'"> Seccion: ' + TransSeccion(i+1).toString() +' </label></td>';
 			}
-			string = string + '</tr><tr><td><b>Mecánica:</b></td>';
+			string = string + '</tr><tr><td><b>5to Año: Mecanica:</b></td>';
 			for ( i = 0; i< data["6"];i++) {
-				string = string + '<td><label class="checkbox-inline"><input type="checkbox" id="6_' + (i+1) +'"> Sección: ' + TransSeccion(i+1).toString() +' </label></td>';
+				string = string + '<td><label class="checkbox-inline"><input type="checkbox" id="6_' + (i+1) +'"> Seccion: ' + TransSeccion(i+1).toString() +' </label></td>';
 			}
-			string = string + "</tr><tr><td><b>Electrónica:</b></td>";
+			string = string + "</tr><tr><td><b>5to Año: Electronica:</b></td>";
 			for ( i = 0; i< data["7"];i++) {
-				string = string + '<td><label class="checkbox-inline"><input type="checkbox" id="7_' + (i+1) +'"> Sección: ' + TransSeccion(i+1).toString() +' </label>';
+				string = string + '<td><label class="checkbox-inline"><input type="checkbox" id="7_' + (i+1) +'"> Seccion: ' + TransSeccion(i+1).toString() +' </label></td>';
 			}
-			string = string + "</tr><tr><td><b>Contabilidad:</b></td>";
+			string = string + "</tr><tr><td><b>5to Año: Contabilidad:</b></td>";
 			for ( i = 0; i< data["8"];i++) {
-				string = string + '<td><label class="checkbox-inline"><input type="checkbox" id="8_' + (i+1) +'"> Sección: ' + TransSeccion(i+1).toString() +' </label></td>';
+				string = string + '<td><label class="checkbox-inline"><input type="checkbox" id="8_' + (i+1) +'"> Seccion: ' + TransSeccion(i+1).toString() +' </label></td>';
 			}
 			string = string + '</tr></table>'		
 			$("#Secciones2").html(string);
-
-			var string = '<table class="table" style="background-color: #FFFFFF;"><tbody><tr><td><b>Tecnología Gráfica:</b></td>'
+			//  Llenamos las secciones de 6to Año 
+			var string = '<table class="table" style="background-color: #FFFFFF;"><tbody><tr><td><b>6to Año: Tecnologia Grafica:</b></td>'
 			for ( i = 0; i< data["9"];i++) {
-				string = string + '<td><label class="checkbox-inline"><input type="checkbox" id="9_' + (i+1) +'"> Sección: ' + TransSeccion(i+1).toString() +' </label></td>';
+				string = string + '<td><label class="checkbox-inline"><input type="checkbox" id="9_' + (i+1) +'"> Seccion: ' + TransSeccion(i+1).toString() +' </label></td>';
 			}
-			string = string + '</tr><tr><td><b>Mecánica:</b></td>';
+			string = string + '</tr><tr><td><b>6to Año: Mecanica:</b></td>';
 			for ( i = 0; i< data["10"];i++) {
-				string = string + '<td><label class="checkbox-inline"><input type="checkbox" id="10_' + (i+1) +'"> Sección: ' + TransSeccion(i+1).toString() +' </label></td>';
+				string = string + '<td><label class="checkbox-inline"><input type="checkbox" id="10_' + (i+1) +'"> Seccion: ' + TransSeccion(i+1).toString() +' </label></td>';
 			}
-			string = string + "</tr><tr><td><b>Electrónica:</b></td>";
+			string = string + "</tr><tr><td><b>6to Año: Electronica:</b></td>";
 			for ( i = 0; i< data["11"];i++) {
-				string = string + '<td><label class="checkbox-inline"><input type="checkbox" id="11_' + (i+1) +'"> Sección: ' + TransSeccion(i+1).toString() +' </label>';
+				string = string + '<td><label class="checkbox-inline"><input type="checkbox" id="11_' + (i+1) +'"> Seccion: ' + TransSeccion(i+1).toString() +' </label></td>';
 			}
-			string = string + "</tr><tr><td><b>Contabilidad:</b></td>";
+			string = string + "</tr><tr><td><b>6to Año: Contabilidad:</b></td>";
 			for ( i = 0; i< data["12"];i++) {
-				string = string + '<td><label class="checkbox-inline"><input type="checkbox" id="12_' + (i+1) +'"> Sección: ' + TransSeccion(i+1).toString() +' </label></td>';
+				string = string + '<td><label class="checkbox-inline"><input type="checkbox" id="12_' + (i+1) +'"> Seccion: ' + TransSeccion(i+1).toString() +' </label></td>';
 			}
 			string = string + '</tr></table>'		
 			$("#Secciones3").html(string);
 
-			var string = '<table class="table" style="background-color: #FFFFFF;"><tbody><tr><td><b>Docente:</b></td>'
-			string = string + '<td><input type="checkbox" id="personal_1"></td>';
-			string = string + '</tr><tr><td><b>Administrativo:</b></td>';
-			string = string + '<td><input type="checkbox" id="personal_2"></td>';
-			string = string + "</tr><tr><td><b>Obrero:</b></td>";
-			string = string + '<td><input type="checkbox" id="personal_3"></td>';
-			
-			string = string + '</tr></table>'		
-			$("#Personal").html(string);
 
 	 	} // Fin Funcion
  });
 
 
 /* 
-		var string = '<ul class="list-inline"><li class="list-group-item"><b>4to Año Tecnología Gráfica:</b></li>'
+		var string = '<ul class="list-inline"><li class="list-group-item"><b>4to Año Tecnologia Grafica:</b></li>'
 			for ( i = 0; i< data["1"];i++) {
-				string = string + '<li class="list-group-item"><label class="checkbox-inline"><input type="checkbox" name=" 1_' + data["1"][i+1] +'"> Sección: ' + TransSeccion(i+1).toString() +' </label></li>';
+				string = string + '<li class="list-group-item"><label class="checkbox-inline"><input type="checkbox" name=" 1_' + data["1"][i+1] +'"> Seccion: ' + TransSeccion(i+1).toString() +' </label></li>';
 			}
-			string = string + '<br/><li class="list-group-item"><b>4to Año Mecánica:</b></li>';
+			string = string + '<br/><li class="list-group-item"><b>4to Año Mecanica:</b></li>';
 			for ( i = 0; i< data["2"];i++) {
-				string = string + '<li class="list-group-item"><label class="checkbox-inline"><input type="checkbox" name=" 1_' + data["2"][i+1] +'"> Sección: ' + TransSeccion(i+1).toString() +' </label></li>';
+				string = string + '<li class="list-group-item"><label class="checkbox-inline"><input type="checkbox" name=" 1_' + data["2"][i+1] +'"> Seccion: ' + TransSeccion(i+1).toString() +' </label></li>';
 			}
-			string = string + "<br/><b>4to Año Electrónica:</b>";
+			string = string + "<br/><b>4to Año Electronica:</b>";
 			for ( i = 0; i< data["3"];i++) {
-				string = string + '<li class="list-group-item"><label class="checkbox-inline"><input type="checkbox" name=" 1_' + data["3"][i+1] +'"> Sección: ' + TransSeccion(i+1).toString() +' </label></li>';
+				string = string + '<li class="list-group-item"><label class="checkbox-inline"><input type="checkbox" name=" 1_' + data["3"][i+1] +'"> Seccion: ' + TransSeccion(i+1).toString() +' </label></li>';
 			}
 			string = string + "<br/><b>4to Año Contabilidad:</b>";
 			for ( i = 0; i< data["4"];i++) {
-				string = string + '<li class="list-group-item"><label class="checkbox-inline"><input type="checkbox" name=" 1_' + data["4"][i+1] +'"> Sección: ' + TransSeccion(i+1).toString() +' </label></li>';
+				string = string + '<li class="list-group-item"><label class="checkbox-inline"><input type="checkbox" name=" 1_' + data["4"][i+1] +'"> Seccion: ' + TransSeccion(i+1).toString() +' </label></li>';
 			}
 			// Llenamos las secciones de 5to Año
-			string = string + "<br/><b>5to Año Tecnología Gráfica:</b>"
+			string = string + "<br/><b>5to Año Tecnologia Grafica:</b>"
 			for ( i = 0; i< data["5"];i++) {
-				string = string + '<label class="checkbox-inline"><input type="checkbox" name=" 1_' + data["5"][i+1] +'"> Sección: ' + TransSeccion(i+1).toString() +' </label>';
+				string = string + '<label class="checkbox-inline"><input type="checkbox" name=" 1_' + data["5"][i+1] +'"> Seccion: ' + TransSeccion(i+1).toString() +' </label>';
 			}
-			string = string + "<br/><b>5to Año Mecánica:</b>";
+			string = string + "<br/><b>5to Año Mecanica:</b>";
 			for ( i = 0; i< data["6"];i++) {
-				string = string + '<label class="checkbox-inline"><input type="checkbox" name=" 1_' + data["6"][i+1] +'"> Sección: ' + TransSeccion(i+1).toString() +' </label>';
+				string = string + '<label class="checkbox-inline"><input type="checkbox" name=" 1_' + data["6"][i+1] +'"> Seccion: ' + TransSeccion(i+1).toString() +' </label>';
 			}
-			string = string + "<br/><b>5to Año Electrónica:</b>";
+			string = string + "<br/><b>5to Año Electronica:</b>";
 			for ( i = 0; i< data["7"];i++) {
-				string = string + '<label class="checkbox-inline"><input type="checkbox" name=" 1_' + data["7"][i+1] +'"> Sección: ' + TransSeccion(i+1).toString() +' </label>';
+				string = string + '<label class="checkbox-inline"><input type="checkbox" name=" 1_' + data["7"][i+1] +'"> Seccion: ' + TransSeccion(i+1).toString() +' </label>';
 			}
 			string = string + "<br/><b>5to Año Contabilidad:</b>";
 			for ( i = 0; i< data["8"];i++) {
-				string = string + '<label class="checkbox-inline"><input type="checkbox" name=" 1_' + data["8"][i+1] +'"> Sección: ' + TransSeccion(i+1).toString() +' </label>';
+				string = string + '<label class="checkbox-inline"><input type="checkbox" name=" 1_' + data["8"][i+1] +'"> Seccion: ' + TransSeccion(i+1).toString() +' </label>';
 			}	
 			// Llenamos las secciones de 6to año
-			string = string + "<br/><b>6to Año Tecnología Gráfica:</b>"
+			string = string + "<br/><b>6to Año Tecnologia Grafica:</b>"
 			for ( i = 0; i< data["9"];i++) {
-				string = string + '<label class="checkbox-inline"><input type="checkbox" name=" 1_' + data["9"][i+1] +'"> Sección: ' + TransSeccion(i+1).toString() +' </label>';
+				string = string + '<label class="checkbox-inline"><input type="checkbox" name=" 1_' + data["9"][i+1] +'"> Seccion: ' + TransSeccion(i+1).toString() +' </label>';
 			}
-			string = string + "<br/><b>6to Año Mecánica:</b>";
+			string = string + "<br/><b>6to Año Mecanica:</b>";
 			for ( i = 0; i< data["10"];i++) {
-				string = string + '<label class="checkbox-inline"><input type="checkbox" name=" 1_' + data["10"][i+1] +'"> Sección: ' + TransSeccion(i+1).toString() +' </label>';
+				string = string + '<label class="checkbox-inline"><input type="checkbox" name=" 1_' + data["10"][i+1] +'"> Seccion: ' + TransSeccion(i+1).toString() +' </label>';
 			}
-			string = string + "<br/><b>6to Año Electrónica:</b>";
+			string = string + "<br/><b>6to Año Electronica:</b>";
 			for ( i = 0; i< data["11"];i++) {
-				string = string + '<label class="checkbox-inline"><input type="checkbox" name=" 1_' + data["11"][i+1] +'"> Sección: ' + TransSeccion(i+1).toString() +' </label>';
+				string = string + '<label class="checkbox-inline"><input type="checkbox" name=" 1_' + data["11"][i+1] +'"> Seccion: ' + TransSeccion(i+1).toString() +' </label>';
 			}
 			string = string + "<br/><b>6to Año Contabilidad:</b>";
 			for ( i = 0; i< data["12"];i++) {
-				string = string + '<label class="checkbox-inline"><input type="checkbox" name=" 1_' + data["12"][i+1] +'"> Sección: ' + TransSeccion(i+1).toString() +' </label>';
+				string = string + '<label class="checkbox-inline"><input type="checkbox" name=" 1_' + data["12"][i+1] +'"> Seccion: ' + TransSeccion(i+1).toString() +' </label>';
 			}
 
 */
